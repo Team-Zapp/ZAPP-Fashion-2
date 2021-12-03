f08e51594c44098f784fc2de663b2830c79d0111
Justro555 <zawphyoaung97@gmail.com>
2021-12-03 16:28:07 +0630

    no message

---
+++ Admin/FeedBack/feedback.js
@@ -0,0 +1,132 @@
+$.get("../../def.html", function (data) {
+  $("body").prepend(data);
+});
+$(document).ready(function () {
+  var count = 0;
+  var rdcolor = "";
+  function makeid() {
+    var characters = "ABCDEF123456789";
+    var charactersLength = characters.length;
+    for (var i = 0; i < 6; i++) {
+      rdcolor += characters.charAt(
+        Math.floor(Math.random() * charactersLength)
+      );
+    }
+    return rdcolor;
+  }
+  $.ajax({
+    type: "get",
+    url: "../../Controllers/FeedbackDatas.php",
+    success: function (data) {
+      let json = JSON.parse(data);
+      console.log(json);
+      for (const x of json) {
+        makeid();
+        let pfname = x.name;
+        pfname = pfname.charAt(0).toUpperCase();
+        count++;
+
+        $("#feedbackForm").append(`
+        <div class="ProflieAndName">
+        <div class="row">
+        <div class="usernfeedback">
+        <div class="UserProfile" style="background-color:#${rdcolor};">${pfname}</div>
+            <p class="name">
+                <button class="btn btn-primary fdbtn" type="button" data-bs-toggle="collapse" data-bs-target="#name${count}" aria-expanded="false" aria-controls="collapseExample">
+                    ${x.name}'s Feedback
+                </button>
+            </p></div>
+            </div>
+            <div class="collapse" id="name${count}">
+                <div class="card card-body fdbody">
+                    ${x.feedback}
+                </div>
+            </div>
+        </div>
+        `);
+        rdcolor = "";
+      }
+    },
+  });
+});
+$.get("../../deflink.html", function (data) {
+  $("head").prepend(data);
+  sessionStorage.setItem("AdminName", AdminName);
+  $("#adminName").text(sessionStorage.getItem("AdminName"));
+  document
+    .getElementById("href1")
+    .setAttribute(
+      "href",
+      "../DashBoard/DashBoard.php?name=" + sessionStorage.getItem("AdminName")
+    );
+  document
+    .getElementById("href2")
+    .setAttribute(
+      "href",
+      "../StockManagement/StockManagement.php?name=" +
+        sessionStorage.getItem("AdminName")
+    );
+  document
+    .getElementById("href3")
+    .setAttribute(
+      "href",
+      "../CustomerInfo/CustomerInfo.php?name=" +
+        sessionStorage.getItem("AdminName")
+    );
+  document
+    .getElementById("href4")
+    .setAttribute(
+      "href",
+      "../Transcation/Transcation.php?name=" +
+        sessionStorage.getItem("AdminName")
+    );
+  document
+    .getElementById("href5")
+    .setAttribute(
+      "href",
+      "../FeedBack/Feedback.php?name=" + sessionStorage.getItem("AdminName")
+    );
+  const d = new Date();
+  let date = d.getDate();
+  let minute = d.getMinutes();
+  let hour = d.getHours();
+
+  localStorage.setItem("LastSigninTime", hour + ":" + minute);
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[d.getMonth()];
+  document.getElementById("date").innerHTML = month + "/" + date;
+
+  if (sessionStorage.getItem("AdminName") == "Zay") {
+    document
+      .getElementById("adminPic")
+      .setAttribute("src", "../AdminPhotos/~Chuky~.jpg");
+  }
+  if (sessionStorage.getItem("AdminName") == "Zar") {
+    document
+      .getElementById("adminPic")
+      .setAttribute("src", "../AdminPhotos/Lets go home.png");
+  }
+  if (sessionStorage.getItem("AdminName") == "Zaw") {
+    document
+      .getElementById("adminPic")
+      .setAttribute("src", "../AdminPhotos/A sunset.jpg");
+  }
+  if (sessionStorage.getItem("AdminName") == "Phu") {
+    document
+      .getElementById("adminPic")
+      .setAttribute("src", "../AdminPhotos/flower.jpg");
+  }
+});
